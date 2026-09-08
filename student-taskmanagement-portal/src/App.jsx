7658da8c0a47cc52731bb04a32f7c5418c0daa49
amulyad9845 <amulyad9845@gmail.com>
2026-09-08 16:26:33 +0530

    JS completed project started!

---
+++ student-taskmanagement-portal/src/App.jsx
@@ -0,0 +1,15 @@
+import Welcome from "./components/Welcme"
+
+function App() {
+  return (
+    <div>
+      <Welcome />
+      <Welcome />
+      <Welcome />
+      <Welcome />
+    </div>
+  )
+}
+
+export default App ;
+
